e80406b91119b51951019b19f56f0748789ab538
Michael Russell <Crazybus@users.noreply.github.com>
2026-03-02 08:58:11 +1000

    fix knights vs pike scenario

--- src/data/scenarios/knights_vs_pikes.ts
+++ src/data/scenarios/knights_vs_pikes.ts
@@ -1,14 +1,17 @@
 export const knights_vs_pikes = {
     "name": "Knights vs Pikes",
-    "desc": "4 barracks pikes to catch up to a player with double stable knights and 8 on the field. Was Nili wrong all along?",
+    "desc": "3 barracks pikes to catch up to a player with double stable knights and 8 on the field. Was Nili wrong all along?",
     "a": {
         "nm": "Knight",
         "c": "20",
         "age": "3",
         "h": "100",
         "am": "10",
+        "aa": "0",
         "ar": "2",
         "rl": "1.7999999523162842",
+        "af": "45",
+        "ag": "25",
         "tl": [
             {
                 "t": "production",
@@ -22,7 +25,16 @@ export const knights_vs_pikes = {
                 "t": "building",
                 "n": "Stable",
                 "d": 0,
-                "c": 2,
+                "c": 1,
+                "co": 175,
+                "prod": true,
+                "i": "stable"
+            },
+            {
+                "t": "building",
+                "n": "Stable",
+                "d": 0,
+                "c": 1,
                 "co": 175,
                 "prod": true,
                 "i": "stable"
@@ -57,12 +69,15 @@ export const knights_vs_pikes = {
         "age": "3",
         "h": "55",
         "am": "4",
+        "ar": "2",
         "rl": "3",
+        "af": "80",
+        "aw": "0",
         "tl": [
             {
                 "t": "production",
                 "n": "Initial Production",
-                "d": 210,
+                "d": 110,
                 "c": 1,
                 "co": 0,
                 "tr": 22
@@ -71,7 +86,25 @@ export const knights_vs_pikes = {
                 "t": "building",
                 "n": "Barracks",
                 "d": 0,
-                "c": 4,
+                "c": 1,
+                "co": 175,
+                "prod": true,
+                "i": "barracks"
+            },
+            {
+                "t": "building",
+                "n": "Barracks",
+                "d": 0,
+                "c": 1,
+                "co": 175,
+                "prod": true,
+                "i": "barracks"
+            },
+            {
+                "t": "building",
+                "n": "Barracks",
+                "d": 0,
+                "c": 1,
                 "co": 175,
                 "prod": true,
                 "i": "barracks"
