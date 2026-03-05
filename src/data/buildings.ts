98588f46898d3df2861598774fc99506c0949c6b
Nick Scarratt <nscarratt@gmail.com>
2026-03-05 14:03:12 +0000

    prettier

--- src/data/buildings.ts
+++ src/data/buildings.ts
@@ -1,474 +1,466 @@
 import { BuildingData } from '../sim/types';
 
 export const buildings: Record<string, BuildingData> = {
-    "archery_range_87": {
-        "name": "Archery Range",
-        "f": 0,
-        "w": 175,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "87",
-        "age": 2,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  archery_range_87: {
+    name: 'Archery Range',
+    f: 0,
+    w: 175,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '87',
+    age: 2,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "barracks_12": {
-        "name": "Barracks",
-        "f": 0,
-        "w": 175,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "12",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  barracks_12: {
+    name: 'Barracks',
+    f: 0,
+    w: 175,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '12',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "blacksmith_103": {
-        "name": "Blacksmith",
-        "f": 0,
-        "w": 150,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "103",
-        "age": 2,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  blacksmith_103: {
+    name: 'Blacksmith',
+    f: 0,
+    w: 150,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '103',
+    age: 2,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "bombard_tower_236": {
-        "name": "Bombard Tower",
-        "f": 0,
-        "w": 0,
-        "g": 100,
-        "s": 125,
-        "time": 50,
-        "id": "236",
-        "age": 4,
-        "requires": {
-            "techs": [
-                64
-            ],
-            "buildings": []
-        }
+  },
+  bombard_tower_236: {
+    name: 'Bombard Tower',
+    f: 0,
+    w: 0,
+    g: 100,
+    s: 125,
+    time: 50,
+    id: '236',
+    age: 4,
+    requires: {
+      techs: [64],
+      buildings: [],
     },
-    "caravanserai_1754": {
-        "name": "Caravanserai",
-        "f": 0,
-        "w": 175,
-        "g": 0,
-        "s": 50,
-        "time": 50,
-        "id": "1754",
-        "age": 4,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  caravanserai_1754: {
+    name: 'Caravanserai',
+    f: 0,
+    w: 175,
+    g: 0,
+    s: 50,
+    time: 50,
+    id: '1754',
+    age: 4,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "castle_82": {
-        "name": "Castle",
-        "f": 0,
-        "w": 0,
-        "g": 0,
-        "s": 650,
-        "time": 50,
-        "id": "82",
-        "age": 3,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  castle_82: {
+    name: 'Castle',
+    f: 0,
+    w: 0,
+    g: 0,
+    s: 650,
+    time: 50,
+    id: '82',
+    age: 3,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "dock_45": {
-        "name": "Dock",
-        "f": 0,
-        "w": 150,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "45",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  dock_45: {
+    name: 'Dock',
+    f: 0,
+    w: 150,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '45',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "farm_50": {
-        "name": "Farm",
-        "f": 0,
-        "w": 60,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "50",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  farm_50: {
+    name: 'Farm',
+    f: 0,
+    w: 60,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '50',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "fish_trap_199": {
-        "name": "Fish Trap",
-        "f": 0,
-        "w": 100,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "199",
-        "age": 2,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  fish_trap_199: {
+    name: 'Fish Trap',
+    f: 0,
+    w: 100,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '199',
+    age: 2,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "fortified_church_1806": {
-        "name": "Fortified Church",
-        "f": 0,
-        "w": 200,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "1806",
-        "age": 3,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  fortified_church_1806: {
+    name: 'Fortified Church',
+    f: 0,
+    w: 200,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '1806',
+    age: 3,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "fortified_wall_155": {
-        "name": "Fortified Wall",
-        "f": 0,
-        "w": 0,
-        "g": 0,
-        "s": 5,
-        "time": 50,
-        "id": "155",
-        "age": 3,
-        "requires": {
-            "techs": [
-                194
-            ],
-            "buildings": []
-        }
+  },
+  fortified_wall_155: {
+    name: 'Fortified Wall',
+    f: 0,
+    w: 0,
+    g: 0,
+    s: 5,
+    time: 50,
+    id: '155',
+    age: 3,
+    requires: {
+      techs: [194],
+      buildings: [],
     },
-    "gate_487": {
-        "name": "Gate",
-        "f": 0,
-        "w": 0,
-        "g": 0,
-        "s": 30,
-        "time": 50,
-        "id": "487",
-        "age": 2,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  gate_487: {
+    name: 'Gate',
+    f: 0,
+    w: 0,
+    g: 0,
+    s: 30,
+    time: 50,
+    id: '487',
+    age: 2,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "guard_tower_234": {
-        "name": "Guard Tower",
-        "f": 0,
-        "w": 35,
-        "g": 0,
-        "s": 125,
-        "time": 50,
-        "id": "234",
-        "age": 3,
-        "requires": {
-            "techs": [
-                140
-            ],
-            "buildings": []
-        }
+  },
+  guard_tower_234: {
+    name: 'Guard Tower',
+    f: 0,
+    w: 35,
+    g: 0,
+    s: 125,
+    time: 50,
+    id: '234',
+    age: 3,
+    requires: {
+      techs: [140],
+      buildings: [],
     },
-    "house_70": {
-        "name": "House",
-        "f": 0,
-        "w": 25,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "70",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  house_70: {
+    name: 'House',
+    f: 0,
+    w: 25,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '70',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "keep_235": {
-        "name": "Keep",
-        "f": 0,
-        "w": 35,
-        "g": 0,
-        "s": 125,
-        "time": 50,
-        "id": "235",
-        "age": 4,
-        "requires": {
-            "techs": [
-                63
-            ],
-            "buildings": []
-        }
+  },
+  keep_235: {
+    name: 'Keep',
+    f: 0,
+    w: 35,
+    g: 0,
+    s: 125,
+    time: 50,
+    id: '235',
+    age: 4,
+    requires: {
+      techs: [63],
+      buildings: [],
     },
-    "lumber_camp_562": {
-        "name": "Lumber Camp",
-        "f": 0,
-        "w": 100,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "562",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  lumber_camp_562: {
+    name: 'Lumber Camp',
+    f: 0,
+    w: 100,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '562',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "market_84": {
-        "name": "Market",
-        "f": 0,
-        "w": 175,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "84",
-        "age": 2,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  market_84: {
+    name: 'Market',
+    f: 0,
+    w: 175,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '84',
+    age: 2,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "mill_68": {
-        "name": "Mill",
-        "f": 0,
-        "w": 100,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "68",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  mill_68: {
+    name: 'Mill',
+    f: 0,
+    w: 100,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '68',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "mining_camp_584": {
-        "name": "Mining Camp",
-        "f": 0,
-        "w": 100,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "584",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  mining_camp_584: {
+    name: 'Mining Camp',
+    f: 0,
+    w: 100,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '584',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "monastery_104": {
-        "name": "Monastery",
-        "f": 0,
-        "w": 175,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "104",
-        "age": 3,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  monastery_104: {
+    name: 'Monastery',
+    f: 0,
+    w: 175,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '104',
+    age: 3,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "mule_cart_1808": {
-        "name": "Mule Cart",
-        "f": 20,
-        "w": 80,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "1808",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  mule_cart_1808: {
+    name: 'Mule Cart',
+    f: 20,
+    w: 80,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '1808',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "outpost_598": {
-        "name": "Outpost",
-        "f": 0,
-        "w": 25,
-        "g": 0,
-        "s": 5,
-        "time": 50,
-        "id": "598",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  outpost_598: {
+    name: 'Outpost',
+    f: 0,
+    w: 25,
+    g: 0,
+    s: 5,
+    time: 50,
+    id: '598',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "palisade_gate_792": {
-        "name": "Palisade Gate",
-        "f": 0,
-        "w": 30,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "792",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  palisade_gate_792: {
+    name: 'Palisade Gate',
+    f: 0,
+    w: 30,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '792',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "palisade_wall_72": {
-        "name": "Palisade Wall",
-        "f": 0,
-        "w": 3,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "72",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  palisade_wall_72: {
+    name: 'Palisade Wall',
+    f: 0,
+    w: 3,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '72',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "settlement_2556": {
-        "name": "Settlement",
-        "f": 0,
-        "w": 125,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "2556",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  settlement_2556: {
+    name: 'Settlement',
+    f: 0,
+    w: 125,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '2556',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "siege_workshop_49": {
-        "name": "Siege Workshop",
-        "f": 0,
-        "w": 200,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "49",
-        "age": 3,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  siege_workshop_49: {
+    name: 'Siege Workshop',
+    f: 0,
+    w: 200,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '49',
+    age: 3,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "stable_101": {
-        "name": "Stable",
-        "f": 0,
-        "w": 175,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "101",
-        "age": 2,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  stable_101: {
+    name: 'Stable',
+    f: 0,
+    w: 175,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '101',
+    age: 2,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "stone_wall_117": {
-        "name": "Stone Wall",
-        "f": 0,
-        "w": 0,
-        "g": 0,
-        "s": 5,
-        "time": 50,
-        "id": "117",
-        "age": 2,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  stone_wall_117: {
+    name: 'Stone Wall',
+    f: 0,
+    w: 0,
+    g: 0,
+    s: 5,
+    time: 50,
+    id: '117',
+    age: 2,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "town_center_109": {
-        "name": "Town Center",
-        "f": 0,
-        "w": 275,
-        "g": 0,
-        "s": 100,
-        "time": 50,
-        "id": "109",
-        "age": 1,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  town_center_109: {
+    name: 'Town Center',
+    f: 0,
+    w: 275,
+    g: 0,
+    s: 100,
+    time: 50,
+    id: '109',
+    age: 1,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "town_center_621": {
-        "name": "Town Center",
-        "f": 0,
-        "w": 275,
-        "g": 0,
-        "s": 100,
-        "time": 50,
-        "id": "621",
-        "age": 3,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  town_center_621: {
+    name: 'Town Center',
+    f: 0,
+    w: 275,
+    g: 0,
+    s: 100,
+    time: 50,
+    id: '621',
+    age: 3,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "university_209": {
-        "name": "University",
-        "f": 0,
-        "w": 200,
-        "g": 0,
-        "s": 0,
-        "time": 50,
-        "id": "209",
-        "age": 3,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  university_209: {
+    name: 'University',
+    f: 0,
+    w: 200,
+    g: 0,
+    s: 0,
+    time: 50,
+    id: '209',
+    age: 3,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "watch_tower_79": {
-        "name": "Watch Tower",
-        "f": 0,
-        "w": 35,
-        "g": 0,
-        "s": 125,
-        "time": 50,
-        "id": "79",
-        "age": 2,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
+  },
+  watch_tower_79: {
+    name: 'Watch Tower',
+    f: 0,
+    w: 35,
+    g: 0,
+    s: 125,
+    time: 50,
+    id: '79',
+    age: 2,
+    requires: {
+      techs: [],
+      buildings: [],
     },
-    "wonder_276": {
-        "name": "Wonder",
-        "f": 0,
-        "w": 1000,
-        "g": 1000,
-        "s": 1000,
-        "time": 50,
-        "id": "276",
-        "age": 4,
-        "requires": {
-            "techs": [],
-            "buildings": []
-        }
-    }
+  },
+  wonder_276: {
+    name: 'Wonder',
+    f: 0,
+    w: 1000,
+    g: 1000,
+    s: 1000,
+    time: 50,
+    id: '276',
+    age: 4,
+    requires: {
+      techs: [],
+      buildings: [],
+    },
+  },
 };
